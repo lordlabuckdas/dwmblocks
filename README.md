16172de92b44c05c0295cb7907fffd5bc6af7cff
lordlabuckdas <55460753+lordlabuckdas@users.noreply.github.com>
2021-05-06 21:17:06 +0530

    init + scripts

---
+++ README.md
@@ -0,0 +1,12 @@
+# dwmblocks
+
+personal arch linux dwmblocks status bar for dwm
+
+## scripts
+
+* power
+* volume
+* network
+* clock
+
+**NOTE:** no patches have been applied
---
+++ blocks.def.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/* {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0}, */
+	{"", "$HOME/dwmblocks/scripts/volume_bar", 30, 0},
+	{"", "$HOME/dwmblocks/scripts/net_bar", 120, 0},
+	{"", "$HOME/dwmblocks/scripts/power_bar", 180, 0},
+	{"", "$HOME/dwmblocks/scripts/clock_bar", 60, 0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
