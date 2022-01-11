22fd87c145c645936fb7839f004bfc9a19a021fc
lordlabuckdas <55460753+lordlabuckdas@users.noreply.github.com>
2022-01-11 19:40:01 +0530

    add date and time blocks

--- blocks.def.h
+++ blocks.def.h
@@ -3,9 +3,11 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0}, */
 	{"", "$HOME/dwmblocks/scripts/volume_bar", 30, 0},
-	{"", "$HOME/dwmblocks/scripts/net_bar", 120, 0},
+	/* {"", "$HOME/dwmblocks/scripts/net_bar", 120, 0}, */
 	{"", "$HOME/dwmblocks/scripts/power_bar", 180, 0},
-	{"", "$HOME/dwmblocks/scripts/clock_bar", 60, 0},
+	/* {"", "$HOME/dwmblocks/scripts/clock_bar", 60, 0}, */
+	{" ", "date '+%b %d, %a'", 60*60, 0},
+	{" ", "date '+%I:%M%p '", 60, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
